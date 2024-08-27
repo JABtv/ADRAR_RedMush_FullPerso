3181b05046cf41be697ecc6925e132fe7d2b7aab
JABtv <tom.andres31000@gmail.com>
2024-08-27 02:14:49 +0200

    [-]Carrousel.js [+]Mise en place de la section article nouveauté

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-const refreshButton = document.getElementById("refreshButton");
-
-function refreshPage() {
-    location.reload();
-}
-
-refreshButton.addEventListener("click", refreshPage);
-
-setInterval(refreshPage, 10000); // Refresh every 10 seconds
